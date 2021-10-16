9734e2f18f2111d1c2b0fa6d07bf4ab5cc8238a1
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-10-16 17:33:01 +0800

    Added  validations for empty input

---
+++ form-validator/script.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//functions
+function showError(input, message){
+    let formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    let small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input){
+    let formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+//event listeners
+form.addEventListener('submit', function submitForm(e){
+    e.preventDefault();
+    if(username.value === ''){
+        showError(username, 'Username is required')
+    } else{
+        showSuccess(username)
+    }
+
+    if(email.value === ''){
+        showError(email, 'Email is required')
+    } else if(!validateEmail(email.value)){
+        showError(email, 'Email is not valid')
+    } 
+    else {
+        showSuccess(email)
+    }
+
+    if(password.value === ''){
+        showError(password, 'Password is required')
+    } else{
+        showSuccess(password)
+    }
+
+    if(password2.value === ''){
+        showError(password2, 'Password is required')
+    } else{
+        showSuccess(password2)
+    }
+})
