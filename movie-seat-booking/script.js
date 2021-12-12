c03c9b03353154783faebd973fbf522878982e52
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-12-12 15:48:03 +0800

    added javascript to populate UI with local storage data

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,17 +1,41 @@
 const container = document.querySelector('.container');
 const movieSelect = document.getElementById('movie')
-const seat = document.querySelectorAll('.row .seat');
+const seats = document.querySelectorAll('.row .seat');
 const total = document.getElementById('total');
 const count = document.getElementById('count');
 
 let ticketPrice = +movieSelect.value;
-
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 function updateCountTotal(){
     const seatSelected = document.querySelectorAll('.row .seat.selected')
+    // copying the selected seats in array and giving index with map function
+    const seatIndex = [...seatSelected].map((seat) => [...seats].indexOf(seat));
+
+    // storing the index in local storage
+    localStorage.setItem('selectedSeatIndex', JSON.stringify(seatIndex));
     count.innerText = seatSelected.length;
     total.innerText = seatSelected.length*ticketPrice;
 }
+//populate UI with localstorage data
 
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeatIndex'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){seat.classList.add('selected')}
+        })
+    }
+    const movieIndex = localStorage.getItem('selectedMovieIndex')
+    if(movieIndex !== null){
+        movieSelect.selectedIndex = movieIndex;
+    }
+    
+
+}
 // seat click event
 
 container.addEventListener('click', (e) => {
@@ -24,5 +48,10 @@ container.addEventListener('click', (e) => {
 //movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +movieSelect.value
+
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateCountTotal();
 })
+
+populateUI();
+updateCountTotal();
